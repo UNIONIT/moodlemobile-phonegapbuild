96540b9fa54295de1f7ce8821b1b7602b49e074d
UNIONIT <it@west.org.uk>
2015-10-14 12:57:20 +0100

    Update main.js

--- core/components/login/main.js
+++ core/components/login/main.js
@@ -53,7 +53,7 @@ angular.module('mm.core.login', [])
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl',
         onEnter: function($state){
-           $state.go('mm_login.credentials',{siteurl: 'http://moodle.west.org.uk:8990/moodle2'});
+           $state.go('mm_login.credentials');
         
     }
     })
